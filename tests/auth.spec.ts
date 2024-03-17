0952213a0a7ce34a8dea049399c35b193b1b0059
v-nvtsk <v@nvtsk.ru>
2024-03-17 20:58:20 +0300

    build: add initial config

---
+++ tests/auth.spec.ts
@@ -0,0 +1,39 @@
+import { expect, test } from "@playwright/test";
+import { AuthPageObject } from "../testObjectModels/authPageModel";
+import { baseURL } from "./url";
+
+console.log("baseURL: ", baseURL);
+
+// const baseURL = "http://localhost:3000";
+
+test.describe("Auth", () => {
+  let authPage: AuthPageObject;
+  const email = "test@test.test";
+  const password = "123456";
+
+  test.beforeEach(async ({ page }) => {
+    authPage = new AuthPageObject(page);
+    await authPage.open();
+    console.log("auth page: ", page.url());
+  });
+
+  test("unauthorized user", async () => {
+    const page = authPage.getPage();
+    await test.step("go to protected page should be redirected to auth page", async () => {
+      await page.goto(`${baseURL}/calendar`);
+      await page.waitForURL(`${baseURL}/auth/signin`);
+      expect(page.url()).toBe(`${baseURL}/auth/signin`);
+    });
+    await test.step("signin", async () => {
+      await authPage.open();
+      await authPage.fillEmail(email);
+      await authPage.fillPassword(password);
+      await authPage.submitSignIn();
+    });
+    await test.step("after authorization should be redirected back on protected path", async () => {
+      console.log("page.url(): ", page.url());
+      await page.waitForURL(`${baseURL}/calendar`);
+      expect(page.url()).toBe(`${baseURL}/calendar`);
+    });
+  });
+});
---
+++ tests/calendar.spec.ts
@@ -0,0 +1,82 @@
+import { test } from "@playwright/test";
+import { getCurrentWeekNumber, getTodayAsObject } from "../src/helpers";
+import { AuthPageObject } from "../testObjectModels/authPageModel";
+import { CalendarPageObject } from "../testObjectModels/calendarPageModel";
+import { baseURL } from "./url";
+
+const calendarBaseURL = `${baseURL}/calendar`;
+
+test.describe("Calendar", async () => {
+  let calendarPage: CalendarPageObject;
+
+  const todayObj = getTodayAsObject();
+  const currentWeek = getCurrentWeekNumber()[1];
+  const testViews = [
+    {
+      name: "year",
+      next: `/year?year=${todayObj.year + 1}`,
+      prev: `/year?year=${todayObj.year - 1}`,
+      today: `/year?year=${todayObj.year}`,
+    },
+    {
+      name: "month",
+      next: `/month?year=${todayObj.year}&month=${todayObj.month + 1}`,
+      prev: `/month?year=${todayObj.year}&month=${todayObj.month - 1}`,
+      today: `/month?year=${todayObj.year}&month=${todayObj.month}`,
+    },
+    {
+      name: "week",
+      next: `/week?year=${todayObj.year}&week=${currentWeek + 1}`,
+      prev: `/week?year=${todayObj.year}&week=${currentWeek - 1}`,
+      today: `/week?year=${todayObj.year}&week=${currentWeek}`,
+    },
+    {
+      name: "date",
+      next: `/date?year=${todayObj.year}&month=${todayObj.month}&date=${todayObj.date + 1}`,
+      prev: `/date?year=${todayObj.year}&month=${todayObj.month}&date=${todayObj.date - 1}`,
+      today: `/date?year=${todayObj.year}&month=${todayObj.month}&date=${todayObj.date}`,
+    },
+  ];
+
+  test.beforeEach(async ({ page }) => {
+    const authPage = new AuthPageObject(page);
+    const email = "test@test.test";
+    const password = "123456";
+    await authPage.open();
+    await authPage.fillEmail(email);
+    await authPage.fillPassword(password);
+    await authPage.submitSignIn();
+    console.log("calendar page 1: ", page.url());
+    await page.waitForURL(baseURL);
+    console.log("calendar page 2: ", page.url());
+    calendarPage = new CalendarPageObject(page);
+    await calendarPage.open();
+    console.log("calendar page 3: ", page.url());
+  });
+
+  testViews.map((view) =>
+    test(`calendar view ${view.name}`, async () => {
+      const page = calendarPage.getPage();
+      await test.step(`open ${view.name} view`, async () => {
+        await calendarPage.changeView(view.name);
+        await page.waitForURL(`${calendarBaseURL}${view.today}`);
+      });
+
+      await test.step(`goto next ${view.name}`, async () => {
+        await calendarPage.goNext();
+        await page.waitForURL(`${calendarBaseURL}${view.next}`);
+      });
+
+      await test.step(`open previous ${view.name}`, async () => {
+        await calendarPage.goPrev();
+        await calendarPage.goPrev();
+        await page.waitForURL(`${calendarBaseURL}${view.prev}`);
+      });
+
+      await test.step(`goto current ${view.name}`, async () => {
+        await calendarPage.goToday();
+        await page.waitForURL(`${calendarBaseURL}${view.today}`);
+      });
+    }),
+  );
+});
