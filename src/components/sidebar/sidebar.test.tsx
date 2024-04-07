f80a11babba67db486874b2df9f6b269d3447d16
v-nvtsk <v@nvtsk.ru>
2024-04-07 18:24:27 +0300

    feat(components): add sidebar

---
+++ src/components/sidebar/sidebar.test.tsx
@@ -0,0 +1,23 @@
+import { render } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "../../store";
+import { Sidebar } from "./sidebar";
+
+describe("Calendar page", () => {
+  it("should render", async () => {
+    const component = await act(async () =>
+      render(
+        <Provider store={store}>
+          <BrowserRouter>
+            <Sidebar onFilter={() => {}} subfilter={{}} className="" />
+            );
+          </BrowserRouter>
+        </Provider>,
+      ),
+    );
+
+    expect(component.container).toBeInTheDocument();
+  });
+});
