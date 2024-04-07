a3edbbfa6cdc9ea4dfc3736e07d5a2e51e5f6d8a
v-nvtsk <v@nvtsk.ru>
2024-04-07 17:25:35 +0300

    feat(helpers): add fetch

---
+++ src/helpers/fetch/fetch-options.ts
@@ -0,0 +1,18 @@
+type Options = {
+  method: string;
+  headers?: { [key: string]: any };
+  body?: string;
+};
+
+export function createFetchOptions(method?: string, headers?: { [key: string]: any } | null, data?: any) {
+  const options: Options = {
+    method: method || "GET",
+  };
+  if (headers) options.headers = headers;
+  if (method && method !== "GET") {
+    options.headers = options.headers || { "Content-Type": "application/json" };
+    options.body = JSON.stringify(data);
+  }
+
+  return options;
+}
