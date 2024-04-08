886bf8b11eafafd3989caa52d8eb0e06ccd94f48
v-nvtsk <v@nvtsk.ru>
2024-04-08 17:52:30 +0300

    feat(pages): add auth page

---
+++ src/app/auth/auth.test.tsx
@@ -0,0 +1,22 @@
+import { render } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
+import { Provider } from "react-redux";
+import { MemoryRouter } from "react-router-dom";
+import { store } from "../../store";
+import { Auth } from "./auth";
+
+describe("Auth page", () => {
+  it("should render", async () => {
+    const component = await act(async () =>
+      render(
+        <Provider store={store}>
+          <MemoryRouter>
+            <Auth />
+          </MemoryRouter>
+        </Provider>,
+      ),
+    );
+
+    expect(component.container).toBeInTheDocument();
+  });
+});
