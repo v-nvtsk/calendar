b94dddcf3be9eb90ca9ddecf6f3f98a543b60bdf
v-nvtsk <v@nvtsk.ru>
2024-04-08 18:00:04 +0300

    feat(pages): add task page

---
+++ src/app/task/task.test.tsx
@@ -0,0 +1,22 @@
+import { render } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "../../store";
+import { TaskView } from "./task";
+
+describe("Task", () => {
+  it("should render", async () => {
+    const container = await act(async () =>
+      render(
+        <Provider store={store}>
+          <BrowserRouter>
+            <TaskView />
+          </BrowserRouter>
+        </Provider>,
+      ),
+    );
+
+    expect(container.container).toBeInTheDocument();
+  });
+});
