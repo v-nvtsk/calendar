33c0e38d9d3df62112ca1785eb0526b15ed27e9f
v-nvtsk <v@nvtsk.ru>
2024-04-19 15:33:54 +0300

    feat(helper): add week helpers

--- src/helpers/generate-pathname.ts
+++ src/helpers/generate-pathname.ts
@@ -1,3 +1,5 @@
+import { getWeekNumber } from "./week";
+
 export type Views = "year" | "month" | "week" | "date" | "list" | "task";
 export const VIEWS: Views[] = ["year", "month", "week", "date", "list"];
 export type Pathnames = {
@@ -15,18 +17,19 @@ export type GetPathArgs = {
   year: number;
   month: number;
   date: number;
+  week?: number;
   id?: string;
   [key: string]: any;
 };
 
-export function generatePathname({ newView, year, month, date, id }: GetPathArgs): string {
+export function generatePathname({ newView, year, month, date, week, id }: GetPathArgs): string {
   const pathnames: Pathnames = {
     year: `/calendar/${newView}?year=${year}`,
     month: `/calendar/${newView}?year=${year}&month=${month}`,
     date: `/calendar/${newView}?year=${year}&month=${month}&date=${date}`,
-    week: `/calendar/${newView}?year=${year}&month=${month}&date=${date}`,
+    week: `/calendar/${newView}?year=${year}&week=${week || getWeekNumber(new Date(year, month, date).getTime())[1]}`,
     list: `/calendar/${newView}?year=${year}&month=${month}&date=${date}`,
-    task: `/../${newView}?id=${id}`,
+    task: `/../${newView}/view?id=${id}`,
   };
   return pathnames[newView];
 }
