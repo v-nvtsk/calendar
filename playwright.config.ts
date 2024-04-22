6fab0248f752c8e077d6f164a273535d4b439270
v-nvtsk <v@nvtsk.ru>
2024-04-22 16:09:27 +0300

    fix(config): define baseURL as imported constant

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, devices } from "@playwright/test";
+import { baseURL } from "./tests/url";
 
 /**
  * Read environment variables from file.
@@ -24,7 +25,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.CI ? "/otus-jsbasic-final-react" : "/",
+    baseURL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -71,7 +72,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "npm run start",
-    url: `http://127.0.0.1:3000${process.env.CI ? "/otus-jsbasic-final-react" : "/"}`,
+    url: baseURL,
     reuseExistingServer: !process.env.CI,
   },
 });
